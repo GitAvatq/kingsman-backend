6b6d0fa8795158c2ce82cd01835bfa58c0b48333
baielldev <bajel8844@gmail.com>
2025-10-12 17:09:11 +0600

    blog-data done

---
+++ src/modules/blog/blog.controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+import { prisma } from "../../config/prisma";
+import { notsuccess, success } from "../../utils/response";
+import { blogSeed } from "./seed";
+
+export class BlogController {
+  getAll = async (req: Request, res: Response) => {
+    try {
+      await prisma.blog.createMany({
+        data: blogSeed,
+        skipDuplicates: true,
+      });
+      const blog = await prisma.blog.findMany();
+      return success(res, blog);
+    } catch (error: any) {
+      return notsuccess(res, error.message);
+    }
+  };
+
+  create = async (req: Request, res: Response) => {
+    const { image, name, description } = req.body;
+    try {
+      const blog = await prisma.blog.create({
+        data: { image, name, description },
+      });
+
+      return success(res, blog, "Blog created");
+    } catch (error: any) {
+      return notsuccess(res, error.message);
+    }
+  };
+
+  update = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { image, name, description } = req.body;
+
+    try {
+      const blog = await prisma.blog.update({
+        where: { id: Number(id) },
+        data: { image, name, description },
+      });
+
+      return success(res, blog, "Blog updated");
+    } catch (error: any) {
+      return notsuccess(res, error.message);
+    }
+  };
+
+  delete = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      await prisma.blog.delete({
+        where: { id: Number(id) },
+      });
+      return success(res, null, "Blog deleted!!!😡");
+    } catch (error: any) {
+      return notsuccess(res, error.message);
+    }
+  };
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import servicesRoute from "../modules/services/services.route";
 import mastersRoute from "../modules/masters/masters.route";
 import rewievsRoute from "../modules/rewievs/rewievs.route";
+import blogRoute from "../modules/blog/blog.route";
 
 const router = Router();
 
 router.use("/services", servicesRoute);
 router.use("/masters", mastersRoute);
 router.use("/rewievs", rewievsRoute);
+router.use("/blog", blogRoute);
 
 export default router;
