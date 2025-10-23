8486c70ce57483fbdb85ba2042f28eda115882f8
GitAvatq <ufigma2@gmail.com>
2025-10-23 16:03:34 +0600

    perf : masters seed modif

--- src/modules/masters/seed.ts
+++ src/modules/masters/seed.ts
@@ -3,36 +3,42 @@ export const masterSeed = [
     name: "Micheal John",
     address: "New York. Street Washington",
     status: "Top Barber",
-    barberImg: "/Jonh-image.jpeg",
+    barberImg:
+      "https://oldboybarbershop.com/sites/default/files/styles/large/public/2024-07/WhatsApp%20Image%202024-07-23%20at%2009.51.51.jpeg?itok=bVV5hxmI",
   },
   {
     name: "Mike Duke",
     address: "New York. Street Crosby",
     status: "Kingsman Barber",
-    barberImg: "/bond-images.jpeg",
+    barberImg:
+      "https://oldboybarbershop.com/sites/default/files/styles/large/public/2025-02/PHOTO-2025-02-17-15-43-18.jpg?itok=ENBYntvu",
   },
   {
     name: "James Jonatan",
     address: "New York. Street Doyers",
     status: "Premium Barber",
-    barberImg: "/blind-image.jpg",
+    barberImg:
+      "https://oldboybarbershop.com/sites/default/files/styles/large/public/2024-06/WhatsApp%20Image%202024-06-13%20at%2021.03.47.jpeg?itok=X0O5Sdx0",
   },
   {
     name: "Bond Ticky",
     address: "New York. Street Broadway",
     status: "Top Barber",
-    barberImg: "/james-images.jpeg",
+    barberImg:
+      "https://oldboybarbershop.com/sites/default/files/styles/large/public/2024-08/WhatsApp%20Image%202024-08-14%20at%2013.59.48.jpeg?itok=T-zxxHlE",
   },
   {
     name: "Blind Mark",
     address: "New York. Street 5th Avenue",
     status: "Top Barber",
-    barberImg: "/micheal-image.jpeg",
+    barberImg:
+      "https://oldboybarbershop.com/sites/default/files/styles/large/public/2024-10/IMG_1076.jpg?itok=HA-kUo-N",
   },
   {
     name: "Pistacheos Avrilues",
     address: "New York. Street Wall",
     status: "Premium Barber",
-    barberImg: "/pistachoes-image.jpeg",
+    barberImg:
+      "https://oldboybarbershop.com/sites/default/files/styles/large/public/2025-02/WhatsApp%20Image%202025-02-05%20at%2010.49.40_11zon.jpeg?itok=n3dt20Za",
   },
 ];
