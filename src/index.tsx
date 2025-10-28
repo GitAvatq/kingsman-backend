307b9c7a941dbe58bed8e707a9a1f839d29e3693
baielldev <bajel8844@gmail.com>
2025-10-28 14:47:15 +0600

    .

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import buildServer from "./app";
+import "dotenv/config";
+
+const server = buildServer();
+const startServer = () => {
+  try {
+    const PORT = process.env.PORT || 4500;
+    server.listen(
+      {
+        port: PORT,
+        host: "0.0.0.0",
+      },
+      () => {
+        console.log(`Server is running on port http://localhost:${PORT}`);
+      }
+    );
+  } catch (error: any) {
+    console.error(`Server crushed, the crush is : ${error}`);
+  }
+};
+
+startServer();
